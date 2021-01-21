374afae1d6a998739f5e82b7989d10b5d199fb93
victorinno777 <68729090+victorinno777@users.noreply.github.com>
2021-01-21 15:09:43 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const osmosis = require('osmosis');
-osmosis
-    .get('www.google.com')
-    .set({'Title': 'title'})   // альтернатива: `.find('title').set('Title')`
-    .data(console.log)  // выведет {'Title': 'Google'}
