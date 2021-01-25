31db2c4d0b4b07f43f29f8c337efefff9f3f5fdc
Victor <victorinno777@gmail.com@>
2021-01-26 00:27:20 +0200

    cors

--- index.js
+++ index.js
@@ -67,8 +67,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.get('/', function (req, res) {
-		res.sendfile(index.html);
+	res.sendfile(index.html);
 });
 
 app.post('/name', async function (req, res) {
